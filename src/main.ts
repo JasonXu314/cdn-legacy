00d5c3f8f6472d21ec687a4c3dba3577ebf23101
Jason Xu <Xinwen31415@gmail.com>
2023-05-04 23:40:15 -0500

    pulling port from env variable

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 	app.useGlobalPipes(new ValidationPipe());
 	app.enableCors({ origin: true });
 
-	await app.listen(5000);
+	await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
 
